27b30009e48250f25cb1c5fe1ce2df214d6b442b
Konstantin <roughnight2@gmail.com>
2018-10-08 01:46:21 +0300

    added the movie controller and the movie class

---
+++ VidlyProject/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyProject.Models;
+
+namespace VidlyProject.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+                {
+                    Name = "Interstellar",
+                    Id = 1
+                };
+                
+            return View(movie);
+        }
+    }
+}
