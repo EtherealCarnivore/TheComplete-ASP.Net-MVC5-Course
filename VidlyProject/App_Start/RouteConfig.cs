1b3e2fe250cc4b0271e986a44dca2bc3ce96c286
Konstantin <roughnight2@gmail.com>
2018-10-20 19:52:20 +0300

    changed the routing to the new standart, added new actions

--- VidlyProject/App_Start/RouteConfig.cs
+++ VidlyProject/App_Start/RouteConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -12,6 +13,8 @@ namespace VidlyProject
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+            
 
             routes.MapRoute(
                 name: "Default",
--- VidlyProject/Controllers/MoviesController.cs
+++ VidlyProject/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Owin.Security.Provider;
 using VidlyProject.Models;
 
 namespace VidlyProject.Controllers
@@ -17,8 +18,37 @@ namespace VidlyProject.Controllers
                     Name = "Interstellar",
                     Id = 1
                 };
-                
+
             return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound(); 
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // called when we navigate to /movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        
     }
 }
